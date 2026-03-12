419915e5ef4efd75a84ad18d37d8f3916760d4bc
Jojokq <eugeniy1337russia@gmail.com>
2026-03-12 13:54:48 +0300

    Update Perlin interpolation function with random vectors on the lattice points

--- src/second_iter/perlin.h
+++ src/second_iter/perlin.h
@@ -3,7 +3,7 @@
 class perlin {
     private:
         static const int point_count = 256;
-        double randfloat[point_count];
+        vec3 randvec[point_count];
         int perm_x[point_count];
         int perm_y[point_count];
         int perm_z[point_count];
@@ -35,10 +35,30 @@ class perlin {
             return accum;
         }
 
+        static double perlin_interpolation(const vec3 c[2][2][2], double u, double v, double w) {
+            auto uu = u*u*(3-2*u);
+            auto vv = v*v*(3-2*v);
+            auto ww = w*w*(3-2*w);
+
+            auto accum = 0.0;
+
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                    for (int k = 0; k < 2; k++) {
+                        vec3 weight_v(u-i, v-j, w-k);
+                        accum +=  (i*uu + (1-uu)*(1-i))
+                                * (j*vv + (1-vv)*(1-j))
+                                * (k*ww + (1-ww)*(1-k))
+                                * dot(c[i][j][k], weight_v);
+                    }
+
+            return accum;
+        }
+
     public:
         perlin() {
             for (int i = 0; i < point_count; i++) {
-                randfloat[i] = random_double();
+                randvec[i] = unit_vector(vec3::random(-1,1));
             }
             perlin_generate_perm(perm_x);
             perlin_generate_perm(perm_y);
@@ -50,27 +70,23 @@ class perlin {
             auto v = p.y() - std::floor(p.y());
             auto w = p.z() - std::floor(p.z());
 
-            u = u*u*(3-2*u);
-            v = v*v*(3-2*v);
-            w = w*w*(3-2*w);
-
             auto i = int(std::floor(p.x()));
             auto j = int(std::floor(p.y()));
             auto k = int(std::floor(p.z()));
 
-            double c[2][2][2];
+            vec3 c[2][2][2];
 
             for (int di = 0; di < 2 ; di++)
                 for (int dj = 0; dj < 2; dj++)
                     for (int dk = 0; dk < 2; dk++){
-                        c[di][dj][dk] = randfloat[
+                        c[di][dj][dk] = randvec[
                             perm_x[(i+di) & 255] ^
                             perm_y[(j+dj) & 255] ^
                             perm_z[(k+dk) & 255]
                         ];
                     }
 
-            return trilinear_interpolation(c, u , v ,w);
+            return perlin_interpolation(c, u , v ,w);
         }
 
 };
