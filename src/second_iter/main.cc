426ef3d410e20a981991a115e92d966f1794f6dc
Jojokq <eugeniy1337russia@gmail.com>
2026-03-12 18:09:59 +0300

    Add implementation of quadrilaterals

--- src/second_iter/main.cc
+++ src/second_iter/main.cc
@@ -145,6 +145,41 @@ hittable_list perlin_spheres() {
     return world;
 }
 
+hittable_list quads() {
+    hittable_list world;
+
+    // Materials
+    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vert_fov = 80;
+    cam.lookfrom = point3(0,0,9);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+    return world;
+}
+
 void hard_cam( hittable_list& world) {
     camera cam;
 
@@ -175,6 +210,7 @@ int main(int argc, char* argv[]) {
         case 2: eazy_cam(checkered_spheres()); break;
         case 3: earth(); break;
         case 4: perlin_spheres(); break;
+        case 5:quads(); break;
     }
 
     // if (strcmp(argv[1], "1") == 0) {
--- src/second_iter/quad.h
+++ src/second_iter/quad.h
@@ -1,20 +1,28 @@
 #pragma once
 
-#include <hittable.h>
+#include "hittable.h"
 
 class quad: public hittable {
     private:
         point3 Q;
         vec3 u;
         vec3 v;
+        vec3 w;
+        vec3 normal;
         shared_ptr<material> mat;
         aabb bbox;
         double delta = 0.0001;
+        double D;
     public:
         quad(const point3& Q, const vec3& u, const vec3& v, shared_ptr<material> mat)
         : Q(Q), u(u), v(v), mat(mat)
         {
-            //
+            auto n = cross(u, v);
+            normal = unit_vector(n);
+            D = dot(normal, Q);
+            w = n / dot(n, n);
+
+            set_bounding_box();
         }
 
         virtual void set_bounding_box() {
@@ -27,7 +35,47 @@ class quad: public hittable {
         aabb bounding_box() const override {return bbox;}
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-            return false; // TODO
+            auto denom = dot(normal, r.direction());
+
+            //No hit if the ray is parallel to the plane
+            if (std::fabs(denom) < 1e-8) {
+                return false;
+            }
+
+            //Return false if the hit point parameter t is outside the ray interval
+            auto t = (D - dot(normal, r.origin()))/ denom;
+            if (!ray_t.contains(t))
+                return false;
+            
+            //Determing if the hit point lies within the planar shape
+            auto intersection = r.at(t);
+            vec3 planar_hitpoint_vector = intersection - Q;
+            auto alpha = dot(w, cross(planar_hitpoint_vector, v));
+            auto beta = dot(w, cross(u, planar_hitpoint_vector));
+
+            if (!is_interior(alpha, beta, rec))
+                return false;
+
+            //Ray hits shape, set the rest of the hit record 
+            rec.t = t;
+            rec.p = intersection;
+            rec.mat = mat;
+            rec.set_frame_normal(r, normal);
+
+            return true;
+
+        }
+
+        virtual bool is_interior(double a, double b, hit_record& rec) const {
+            interval unit_interval = interval(0, 1);
+            // Given the hit point in plane coordinates, return false if it is outside the
+            // primitive, otherwise set the hit record UV coordinates and return true.
+
+            if ((!unit_interval.contains(a)) || (!unit_interval.contains(b)))
+                return false;
+            rec.u = a;
+            rec.v = b;
+            return true;
         }
 
 };
